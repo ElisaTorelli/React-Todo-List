a1c8de534779eaee92c21f3fd9ec0ace66b4df3b
ElisaTorelli <elisa.torelli1@gmail.com>
2022-07-12 10:26:25 +0200

    project setup

---
+++ react-app/src/components/LoginCard/LoginCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../LoginCard/loginCard.module.css'
+
+const LoginCard = () => {
+    return (
+        <div className={styles.cardContainer}>
+
+        </div>
+    )
+}
+
+export default LoginCard
